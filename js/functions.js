90f259b0d109d9460b5917e79cb93761e8fa505a
Shedninja6 <eli2233@msn.com>
2018-03-19 14:39:37 -0400

    Created dashboard with courses [Mobile]

--- js/functions.js
+++ js/functions.js
@@ -1,1 +1,11 @@
-$( document ).ready(function(){})
+
+//console.dir(document);
+var courseContainer = document.querySelector(".course-container");
+var courseList = courseContainer.getElementsByClassName("course-box");
+addCourse();
+
+function addCourse(){
+  for(i = 0; i < 3; i++){
+    courseContainer.appendChild(courseList[i].cloneNode(true));
+  }
+}
